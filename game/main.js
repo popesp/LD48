59f6246e7c20649f00862b8f4f7fcdfe82804e76
Daniel Goresht <danielgoresht@gmail.com>
2021-04-24 20:24:19 -0600

    more sprite frames

--- game/main.js
+++ game/main.js
@@ -329,6 +329,7 @@ document.addEventListener("DOMContentLoaded", function()
 					"assets/dude2.png",
 					{frameWidth: 32, frameHeight: 32}
 				);
+				console.log(this.load.spritesheet);
 			},
 			create: function()
 			{
@@ -401,20 +402,20 @@ document.addEventListener("DOMContentLoaded", function()
 
 				this.anims.create({
 					key: "left",
-					frames: this.anims.generateFrameNumbers("dude", { start: 0, end: 3 }),
+					frames: this.anims.generateFrameNumbers("dude", { start: 0, end: 6 }),
 					frameRate: 10,
 					repeat: -1
 				});
 
 				this.anims.create({
 					key: "turn",
-					frames: [ { key: "dude", frame: 4 } ],
+					frames: [ { key: "dude", frame: 7 } ],
 					frameRate: 20
 				});
 
 				this.anims.create({
 					key: "right",
-					frames: this.anims.generateFrameNumbers("dude", { start: 5, end: 8 }),
+					frames: this.anims.generateFrameNumbers("dude", { start: 8, end:  13}),
 					frameRate: 10,
 					repeat: -1
 				});
