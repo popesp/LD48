f0040f20768784346d9a08aad2edfa5c7cd9baa0
Vishnu Govindaraj <vishnu2929@gmail.com>
2021-04-25 20:05:04 -0600

    fall damage

--- game/main.js
+++ game/main.js
@@ -15,7 +15,19 @@ const RUN_ACCEL = 0.4;
 const RUN_DECEL = 0.4;
 const MAX_SPEED = 2.4;
 const GRAVITY = 0.15;
+const FALL_DMG_THRESHOLD = 5;
 
+//PLAYER VARIABLES
+const ENERGY_MAX = 5;
+const shovel = {
+	level: 1,
+	dig_energy: 3
+};
+
+let minerals = 0;
+let energy_display = "";
+let mineral_display = "";
+let bar = "";
 
 const map_tile = {
 	"111111111": {frame: "solid", flipped: false},
@@ -276,15 +288,7 @@ const map_tile = {
 	"000010000": {frame: "island", flipped: false}
 };
 
-let energy_max = 10;
-let minerals = 0;
-const shovel = {
-	level: 1,
-	dig_energy: 3
-};
-
-
-function handleCollision(player, level)
+function handleCollision(player, level, scene)
 {
 	function collideTile(index_row, index_col)
 	{
@@ -295,6 +299,13 @@ function handleCollision(player, level)
 
 		if(player.sprite.y - EPSILON > y_top && player.y_old - EPSILON <= y_top)
 		{
+			if(player.yvel > FALL_DMG_THRESHOLD)
+			{
+				const old_energy = scene.data.values.energy_current;
+				scene.data.set("energy_current", Math.round(scene.data.values.energy_current - (player.yvel - FALL_DMG_THRESHOLD)));
+				setEnergy(scene, player, scene.data.values.energy_current, old_energy);
+			}
+
 			player.falling = false;
 			player.yvel = 0;
 			player.sprite.y = y_top;
@@ -449,61 +460,7 @@ document.addEventListener("DOMContentLoaded", function()
 
 			create: function()
 			{
-				const level = generate(0.5, 0.1);
-				level.images = [];
-				for(let index_row = 0; index_row < level.height; ++index_row)
-				{
-					const row_images = [];
-					for(let index_col = 0; index_col < level.width; ++index_col)
-					{
-						let frame = "void", flipped = false;
-						if(level.tiles[index_row][index_col] === -1)
-							frame = "bedrock";
-						else if(level.tiles[index_row][index_col])
-						{
-							const t = map_tile[getSurrounding(level, index_row, index_col)];
-							frame = t.frame;
-							flipped = t.flipped;
-						}
-
-						const tile = this.add.image(index_col*SIZE_TILE, index_row*SIZE_TILE, "tiles", frame);
-						tile.setOrigin(0, 0);
-						tile.setDisplaySize(SIZE_TILE, SIZE_TILE);
-
-						tile.flipX = flipped;
-
-						row_images.push(tile);
-					}
-
-					level.images.push(row_images);
-				}
-				this.data.set("level", level);
-
 				music = this.sound.add("music");
-				music.loop = true;
-				music.play();
-
-
-				const parent = this;
-				let home_open = false;
-				let bag_open = false;
-
-				const screenCenterX = this.cameras.main.worldView.x + this.cameras.main.width / 2;
-				const screenCenterY = this.cameras.main.worldView.y + this.cameras.main.height / 2;
-
-				this.input.gamepad.start();
-				this.data.set("cursors", this.input.keyboard.createCursorKeys());
-
-				this.data.set("emitter", this.add.particles("tiles", "morsel_dirt").createEmitter({
-					speed: {min: 20, max: 100},
-					angle: {min: 200, max: 340},
-					alpha: {start: 1, end: 0},
-					scale: 3,
-					blendMode: "NORMAL",
-					on: false,
-					lifespan: 1000,
-					gravityY: 300
-				}));
 
 				const player = {
 					falling: true,
@@ -515,13 +472,25 @@ document.addEventListener("DOMContentLoaded", function()
 					facing: "right",
 					sprite: this.add.sprite(132, 132, "dude")
 				};
-				player.sprite.setOrigin(0.5, 1);
-				player.sprite.setDisplaySize(20, 16);
-				this.cameras.main.startFollow(player.sprite);
-				this.cameras.main.setBounds(0, 0, level.width*SIZE_TILE, level.height*SIZE_TILE);
-				this.data.set("player", player);
 
-				mineral_emitter = this.add.particles("tiles", "morsel_gold").createEmitter({
+				this.data.set("energy_max", ENERGY_MAX);
+
+				energy_display = this.add.text(84, 16, "Energy:" + this.data.values.energy_max, {fontSize: "12px", fill: "#000"});
+				energy_display.setScrollFactor(0);
+				energy_display.depth = 4;
+				mineral_display = this.add.text(240, 8, minerals, {fontSize: "12px", fill: "#fff", stroke: "#000", strokeThickness: 1});
+				mineral_display.setScrollFactor(0);
+				mineral_display.depth = 4;
+
+				restart_level(this, player);
+
+				const parent = this;
+				let home_open = false;
+				let bag_open = false;
+
+				this.input.gamepad.start();
+				this.data.set("cursors", this.input.keyboard.createCursorKeys());
+				this.data.set("emitter", this.add.particles("tiles", "morsel_dirt").createEmitter({
 					speed: {min: 20, max: 100},
 					angle: {min: 200, max: 340},
 					alpha: {start: 1, end: 0},
@@ -530,42 +499,39 @@ document.addEventListener("DOMContentLoaded", function()
 					on: false,
 					lifespan: 1000,
 					gravityY: 300
-				});
+				}));
 
-				level.images_minerals = [];
-				for(let index_gem = 0; index_gem < level.gems.length; ++index_gem)
-				{
-					const gem = level.gems[index_gem];
-					const image = this.add.image(gem.index_col*SIZE_TILE, gem.index_row*SIZE_TILE, "tiles", "mineral").setOrigin(0, 0).setDisplaySize(SIZE_TILE, SIZE_TILE);
-					level.images_minerals.push(image);
-				}
+
+				const screenCenterX = this.cameras.main.worldView.x + this.cameras.main.width / 2;
+				const screenCenterY = this.cameras.main.worldView.y + this.cameras.main.height / 2;
+
+				this.data.set("mineral_emitter", this.add.particles("tiles", "morsel_gold").createEmitter({
+					speed: {min: 20, max: 100},
+					angle: {min: 200, max: 340},
+					alpha: {start: 1, end: 0},
+					scale: 3,
+					blendMode: "NORMAL",
+					on: false,
+					lifespan: 1000,
+					gravityY: 300
+				}));
 
 				const barbg = this.add.graphics();
 				barbg.fillStyle(0xcc2418, 1);
 				barbg.fillRect(14, 14, 204, 19);
 				barbg.setScrollFactor(0);
-
-				bar = this.add.graphics();
-				bar.fillStyle(0xebb134, 1);
-				bar.displayOriginX = 16;
-
-				bar.fillRect(16, 16, 200, 15);
-				bar.setScrollFactor(0);
-
-				energy_current = energy_max;
-				energy_display = this.add.text(84, 16, "Energy:" + energy_current, {fontSize: "12px", fill: "#000"});
-				energy_display.setScrollFactor(0);
+				barbg.depth = 2;
 
 				mineral_slot = this.add.image(240, 20, "mineral_slot");
 				mineral_slot.setScrollFactor(0);
 				mineral_slot.scale = 0.3;
-				mineral_display = this.add.text(240, 8, minerals, {fontSize: "12px", fill: "#fff", stroke: "#000", strokeThickness: 1});
-				mineral_display.setScrollFactor(0);
+				mineral_slot.depth = 2;
 
 				const button_home = this.add.image(372, 23, "button_home").setInteractive();
 				button_home.setScrollFactor(0);
 				button_home.scale = 0.3;
 				button_home.scaleY = button_home.scaleX;
+				button_home.depth = 2;
 				button_home.on("pointerup", function()
 				{
 					if(!home_open)
@@ -618,6 +584,7 @@ document.addEventListener("DOMContentLoaded", function()
 				button_bag.setScrollFactor(0);
 				button_bag.scale = 0.3;
 				button_bag.scaleY = button_home.scaleX;
+				button_bag.depth = 2;
 				button_bag.on("pointerup", function()
 				{
 					if(!bag_open)
@@ -703,6 +670,8 @@ document.addEventListener("DOMContentLoaded", function()
 				const player = this.data.get("player");
 				const level = this.data.get("level");
 				const emitter = this.data.get("emitter");
+				const mineral_emitter = this.data.get("mineral_emitter");
+
 				//shawns a nerd
 				if(jump && !player.falling)
 				{
@@ -773,7 +742,7 @@ document.addEventListener("DOMContentLoaded", function()
 					player.xvel = 0;
 				}
 
-				handleCollision(player, level);
+				handleCollision(player, level, this);
 
 				const index_row_under = Math.floor(player.sprite.y/SIZE_TILE);
 				const index_col_left = Math.floor((player.sprite.x - WIDTH_PLAYER/2)/SIZE_TILE);
@@ -789,7 +758,7 @@ document.addEventListener("DOMContentLoaded", function()
 					const index_row = Math.floor((player.sprite.y - EPSILON)/SIZE_TILE);
 					const index_col = Math.floor(player.sprite.x/SIZE_TILE) + (player.facing === "right" ? 1 : -1);
 
-					if(dig(level, emitter, mineral_emitter, index_row + (level.tiles[index_row][index_col] ? 0 : 1), index_col, this))
+					if(dig(level, emitter, mineral_emitter, index_row + (level.tiles[index_row][index_col] ? 0 : 1), index_col, this, player))
 					{
 						player.cooldown_dig = COOLDOWN_DIG;
 						player.sprite.anims.play("dig");
@@ -799,7 +768,7 @@ document.addEventListener("DOMContentLoaded", function()
 		}
 	});
 
-	function dig(level, emitter, mineral_emitter, index_row, index_col, scene_instance)
+	function dig(level, emitter, mineral_emitter, index_row, index_col, scene, player)
 	{
 		if(index_row < 0 || index_row >= level.height || index_col < 0 || index_col >= level.width)
 			return false;
@@ -845,6 +814,7 @@ document.addEventListener("DOMContentLoaded", function()
 
 				minerals += 10;
 				mineral_display.setText(minerals);
+
 			}
 		}
 
@@ -859,17 +829,13 @@ document.addEventListener("DOMContentLoaded", function()
 		else
 			emitter.explode(20, x_particle, y_particle);
 
-		energy_current --;
-		energy_display.setText("Energy:" + energy_current);
-		bar.scaleX = energy_current/energy_max;
-		//x offset
-		bar.x += 16 * (1/energy_max);
-		if(energy_current === 0)
+		old_energy = scene.data.values.energy_current;
+		scene.data.set("energy_current", --scene.data.values.energy_current);
+		if(scene.data.values.energy_current <= 0)
 		{
-			restart_scene(scene_instance, "main");
-			minerals = 0;
+			restart_level(scene, player);
 		}
-
+		setEnergy(scene, player, scene.data.values.energy_current, old_energy);
 
 		return true;
 	}
@@ -878,7 +844,7 @@ document.addEventListener("DOMContentLoaded", function()
 	resize();
 });
 
-function upgrade_shovel(scene)
+function upgrade_shovel(scene, player)
 {
 	if(minerals >= 50 && shovel.level < 3)
 	{
@@ -886,26 +852,115 @@ function upgrade_shovel(scene)
 		shovel.dig_energy--;
 		minerals -= 50;
 	}
-	restart_scene(scene, "main");
+	restart_level(scene, player);
 	return shovel;
 }
 
-function upgrade_energy(scene)
+function upgrade_energy(scene, player)
 {
 	if(minerals >= 20)
 	{
-		energy_max += 10;
+		scene.data.set("energy_max", (scene.data.values.energy_max + 10));
+		scene.data.set("energy_current", (scene.data.values.energy_max + 10));
+		const old_energy = scene.data.values.energy_current;
+		setEnergy(scene, player, scene.data.values.energy_max, old_energy);
 		minerals -= 20;
 	}
 
-	restart_scene(scene, "main");
-	return energy_max;
+	restart_level(scene, player);
+	return scene.data.values.energy_max;
 }
 
-function restart_scene(scene_instance, key)
+function restart_level(scene, player)
 {
-	scene_instance.scene.scene.registry.destroy();
-	scene_instance.scene.scene.events.off();
-	scene_instance.scene.start(key);
-	music.stop();
+	if(scene.group_world !== undefined)
+	{
+		scene.group_world.destroy(true);
+		delete scene.group_world;
+	}
+	bar.scaleX = 0;
+	bar.x = 0;
+	minerals = 0;
+	mineral_display.setText(minerals);
+
+	scene.group_world = scene.add.group();
+	console.log("scene", scene);
+
+	const level = generate(0.5, 0.1);
+	level.images = [];
+	for(let index_row = 0; index_row < level.height; ++index_row)
+	{
+		const row_images = [];
+		for(let index_col = 0; index_col < level.width; ++index_col)
+		{
+			let frame = "void", flipped = false;
+			if(level.tiles[index_row][index_col] === -1)
+				frame = "bedrock";
+			else if(level.tiles[index_row][index_col])
+			{
+				const t = map_tile[getSurrounding(level, index_row, index_col)];
+				frame = t.frame;
+				flipped = t.flipped;
+			}
+
+			const tile = scene.add.image(index_col*SIZE_TILE, index_row*SIZE_TILE, "tiles", frame);
+			scene.group_world.add(tile);
+			tile.setOrigin(0, 0);
+			tile.setDisplaySize(SIZE_TILE, SIZE_TILE);
+
+			tile.flipX = flipped;
+
+			row_images.push(tile);
+		}
+
+		level.images.push(row_images);
+	}
+
+	player.sprite.setOrigin(0.5, 1);
+	player.sprite.setDisplaySize(16, 16);
+	player.sprite.depth = 1;
+	scene.cameras.main.startFollow(player.sprite);
+	player.sprite.setPosition(132, 132);
+	scene.data.set("player", player);
+	scene.cameras.main.setBounds(0, 0, level.width*SIZE_TILE, level.height*SIZE_TILE);
+
+	scene.data.set("level", level);
+
+	bar = scene.add.graphics();
+	bar.depth = 3;
+	bar.fillStyle(0xebb134, 1);
+	bar.displayOriginX = 16;
+	bar.fillRect(16, 16, 200, 15);
+	bar.setScrollFactor(0);
+
+	old_energy = scene.data.values.energy_current;
+	scene.data.set("energy_current", (scene.data.values.energy_current && scene.data.values.energy_current > 0) ? scene.data.values.energy_current : scene.data.values.energy_max);
+	setEnergy(scene, player, scene.data.values.energy_current, old_energy);
+
+	music.loop = true;
+	music.play();
+
+	level.images_minerals = [];
+	for(let index_gem = 0; index_gem < level.gems.length; ++index_gem)
+	{
+		const gem = level.gems[index_gem];
+		const image = scene.add.image(gem.index_col*SIZE_TILE, gem.index_row*SIZE_TILE, "tiles", "mineral").setOrigin(0, 0).setDisplaySize(SIZE_TILE, SIZE_TILE);
+		scene.group_world.add(image);
+		level.images_minerals.push(image);
+	}
+}
+
+function setEnergy(scene, player, energy, old_energy)
+{
+	if(energy <= 0)
+	{
+		restart_level(scene, player);
+	}
+	else
+	{
+		energy_display.setText("Energy: " + energy);
+		bar.scaleX = scene.data.values.energy_current/scene.data.values.energy_max;
+		bar.x += (scene.data.values.energy_max !== scene.data.values.energy_current) ? 16 * ((old_energy-energy)/scene.data.values.energy_max) : 0;
+	}
+
 }
